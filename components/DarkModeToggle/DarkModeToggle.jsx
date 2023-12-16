44dd1243fb932469a7e9f86ff275dbe2f311283e
marcushovi <hovimarcus@gmail.com>
2023-12-16 23:48:25 +0100

    edited dark mode switch style

--- components/DarkModeToggle/DarkModeToggle.jsx
+++ components/DarkModeToggle/DarkModeToggle.jsx
@@ -18,9 +18,10 @@ export default function DarkModeToggle() {
       onClick={() =>
         setColorScheme(computedColorScheme === "light" ? "dark" : "light")
       }
-      variant="default"
+      variant="light"
       size="lg"
       aria-label="Toggle color scheme"
+      color={computedColorScheme === "light" ? "violet" : "yellow"}
     >
       <IconSun className={cx(classes.icon, classes.light)} stroke={1.5} />
       <IconMoon className={cx(classes.icon, classes.dark)} stroke={1.5} />
